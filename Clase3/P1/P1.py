5e9ced98dcc726c2b0b42af7e4e3da0f41200a60
NonGlory <luisalberst@gmail.com>
2022-07-14 13:20:11 -0400

    Creacion de directorios por clase, y Agrego de Clase 3 / P1 como ejercicio

---
+++ Clase3/P1/P1.py
@@ -0,0 +1,22 @@
+# Datos de un contacto
+# Creacion de variables 
+nombres = 'Luis Alberto'
+apellidos = 'Hernandez Chavez'
+edad = 22
+email = 'luisalberst@gmail.com'
+telefono = '+58-412-6613207'
+casado = False
+con_hijos = False
+lista_de_amigos = ['Eduardo', 'Oscar', 'Manuel']
+peliculas_vistas = {'pelicula':'Progressive', 'pelicula-1':'Free Guy', 'pelicula-2':'Kimi na wa'}
+
+# Impresion de las variables en consola o pantalla
+print(nombres)
+print(apellidos)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(con_hijos)
+print(lista_de_amigos)
+print(peliculas_vistas)
