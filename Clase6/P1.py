a6d8e5a03233b5455189aadb8440106808388fca
NonGlory <luisalberst@gmail.com>
2022-07-18 01:20:37 -0400

    Clase 6 - P1 (Actualizado)

--- Clase6/P1.py
+++ Clase6/P1.py
@@ -1,7 +1,10 @@
 class Vehiculo:
-    Color = 'Negro'
-    Ruedas = '4'
-    Puertas = '4'
+
+    def vehiculo(self, color, ruedas, puertas):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+        print('El vehiculo es de color {}, tiene {} ruedas y {} puertas'.format(self.color, self.ruedas, self.puertas))
 
 class Coche(Vehiculo):
     Velocidad = '100 Km/h'
@@ -9,8 +12,6 @@ class Coche(Vehiculo):
 
 c = Coche()
 
-print('El color es',c.Color)
-print('Tiene',c.Ruedas,'ruedas')
-print('Tiene',c.Puertas,'puertas')
+c.vehiculo('negro','4','4')
 print('Alcanza una velocidad de',c.Velocidad)
 print('Posee una cilindrada de',c.cilindrada)
